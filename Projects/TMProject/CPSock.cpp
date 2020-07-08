2a137a8abb2d949206da4cd474278f3c6ea9e9f4
ericsantosdc <eric_santos2009@hotmail.com>
2020-07-07 23:42:41 -0300

    CPSock started

---
+++ Projects/TMProject/CPSock.cpp
@@ -0,0 +1,114 @@
+#include "pch.h"
+#include "CPSock.h"
+#include <WinSock2.h>
+
+CPSock::CPSock()
+{
+	Sock = NULL;
+	Init = 0;
+	pSendBuffer = (char*)malloc(SEND_BUFFER_SIZE);
+	pRecvBuffer = (char*)malloc(RECV_BUFFER_SIZE);
+
+	memset(pSendBuffer, 0, SEND_BUFFER_SIZE);
+	memset(pRecvBuffer, 0, RECV_BUFFER_SIZE);
+
+	nSendPosition = 0;
+	nSentPosition = 0;
+	nRecvPosition = 0;
+	nProcPosition = 0;
+	memset(SendQueue, 0, MAX_KEYWORD_QUEUE);
+	memset(RecvQueue, 0, MAX_KEYWORD_QUEUE);
+	SendCount = 0;
+	RecvCount = 0;
+	ErrCount = 0;
+}
+
+CPSock::~CPSock()
+{
+	if (pSendBuffer)
+	{
+		free(pSendBuffer);
+		pSendBuffer = NULL;
+	}
+	if (pRecvBuffer)
+	{
+		free(pRecvBuffer);
+		pRecvBuffer = NULL;
+	}
+}
+
+bool CPSock::WSAInitialize()
+{
+	WSAData WSAData;
+
+	return WSAStartup(MAKEWORD(1, 1), &WSAData) == 0;
+}
+
+unsigned int CPSock::StartListen(HWND hWnd, int ip, int port, int WSA)
+{
+	sockaddr_in local_sin;
+	memset((char*)&local_sin, 0, sizeof(local_sin));
+
+	char Temp[256];
+	memset(Temp, 0, sizeof(Temp));
+
+	unsigned int tSock = socket(2, 1, 0);
+	
+	if (tSock == -1)
+	{
+		MessageBoxA(hWnd, "Initialize socket fail", "ERROR", 0);
+		return 0;
+	}
+
+	gethostname(Temp, 256);
+	local_sin.sin_family = AF_INET;
+	local_sin.sin_addr.S_un.S_addr = ip;
+	local_sin.sin_port = htons(port);
+
+	if (bind(tSock, (const struct sockaddr*) & local_sin, 16) == -1)
+	{
+		MessageBoxA(hWnd, "Binding fail", "ERROR", 0);
+		closesocket(tSock);
+		return 0;
+	}
+	else if (listen(tSock, 8) >= 0)
+	{
+		if (WSAAsyncSelect(tSock, hWnd, WSA, 8) <= 0)
+		{
+			Sock = tSock;
+			return tSock;
+		}
+		else
+		{
+			MessageBoxA(hWnd, "WSAAsyncSelect fail", "ERROR", 0);
+			closesocket(tSock);
+			return 0;
+		}
+	}
+	else
+	{
+		MessageBoxA(hWnd, "Listen fail", "ERROR", 0);
+		closesocket(tSock);
+		return 0;
+	}
+}
+
+unsigned int CPSock::ConnectServer(char* HostAddr, int Port, int ip, int WSA)
+{
+	return 0;
+}
+
+unsigned int CPSock::SingleConnect(char* HostAddr, int Port, int ip, int WSA)
+{
+	return 0;
+}
+
+int CPSock::Receive()
+{
+	return 0;
+}
+
+char* CPSock::ReadMessage(int* ErrorCode, int* ErrorType)
+{
+	return nullptr;
+}
---
+++ Projects/TMProject/CPSock.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#define RECV_BUFFER_SIZE 131072
+#define SEND_BUFFER_SIZE 131072
+
+#define MAX_KEYWORD_QUEUE 16
+
+class CPSock
+{
+public:
+	CPSock();
+	~CPSock();
+
+	bool WSAInitialize();
+	unsigned int StartListen(HWND* hWnd, int ip, int port, int WSA);
+	unsigned int ConnectServer(char* HostAddr, int Port, int ip, int WSA);
+	unsigned int SingleConnect(char* HostAddr, int Port, int ip, int WSA);
+	int Receive();
+	char* ReadMessage(int* ErrorCode, int* ErrorType);
+	int CloseSocket();
+	int AddMessage(char* pMsg, int Size);
+	int AddMessage_0(char* pMsg, int Size, int FixedKeyWord);
+	bool SendMessageA();
+	int SendOneMessage(char* Msg, int Size);
+	int SendOneMessageKeyword(char* Msg, int Size, int Keyword);
+	int AddMessage2(char* pMsg, int Size);
+	char* ReadMessage2(int* ErrorCode, int* ErrorType);
+	void RefreshRecvBuffer();
+	void RefreshSendBuffer();
+
+private:
+	unsigned int Sock;
+	char* pSendBuffer;
+	char* pRecvBuffer;
+	int nSendPosition;
+	int nRecvPosition;
+	int nProcPosition;
+	int nSentPosition;
+	int Init;
+
+	char SendQueue[MAX_KEYWORD_QUEUE];
+	char RecvQueue[MAX_KEYWORD_QUEUE];
+
+	int SendCount;
+	int RecvCount;
+	int ErrCount;
+};
