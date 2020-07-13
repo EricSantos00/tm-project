f12de0b79a6457fab74cb4c1b725eeda45b73128
ericsantosdc <eric_santos2009@hotmail.com>
2020-07-13 18:02:16 -0300

    dsutil(SoundManagement) and TMLog methods and members created

--- Projects/TMProject/TMLog.h
+++ Projects/TMProject/TMLog.h
@@ -1,1 +1,9 @@
 #pragma once
+
+char LOG_INITIALIZELOG(char* szLogFile);
+void LOG_WRITELOG(const char* lpszFormat, ...);
+void LOG_WRITELOG(unsigned int dwError);
+void LOG_WRITESYSERROR(unsigned int dwErrorIndex, unsigned int dwErrorCode);
+void LOG_WRITEERROR(unsigned int dwErrorIndex);
+void LOG_WRITELOGSTRING(const char* lpszFormat, ...);
+void LOG_FINALIZELOG();
