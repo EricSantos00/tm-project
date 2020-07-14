d9c73b9ec812f5bdcc698e3aaea50179ee0cf32e
ericsantosdc <eric_santos2009@hotmail.com>
2020-07-14 12:32:24 -0300

    Base structures

--- Projects/TMProject/Basedef.h
+++ Projects/TMProject/Basedef.h
@@ -10,5 +10,634 @@ struct MSG_STANDARD
 	unsigned int Tick;
 };
 
+struct STRUCT_SCORE
+{
+	short Level;
+	int Ac;
+	int Damage;
+	char Reserved;
+	char AttackRun;
+	int MaxHp;
+	int MaxMp;
+	int Hp;
+	int Mp;
+	short Str;
+	short Int;
+	short Dex;
+	short Con;
+	unsigned short Special[4];
+};
+
+
+union STRUCT_BONUSEFFECT
+{
+	char cEffect;
+	char cValue;
+	short sValue;
+};
+
+struct STRUCT_ITEM
+{
+	short sIndex;
+	STRUCT_BONUSEFFECT stEffect[3];
+};
+
+struct STRUCT_SELCHAR
+{
+	unsigned short HomeTownX[4];
+	unsigned short HomeTownY[4];
+	char MobName[4][16];
+	STRUCT_SCORE Score[4];
+	STRUCT_ITEM Equip[4][16];
+	unsigned short Guild[4];
+	int Coin[4];
+	long long Exp[4];
+};
+
+struct STRUCT_MOB
+{
+	char MobName[16];
+	char Clan;
+	char Merchant;
+	unsigned short Guild;
+	char Class;
+	char Rsv;
+	unsigned short Quest;
+	int Coin;
+	long long Exp;
+	unsigned short HomeTownX;
+	unsigned short HomeTownY;
+	STRUCT_SCORE BaseScore;
+	STRUCT_SCORE CurrentScore;
+	STRUCT_ITEM Equip[16];
+	STRUCT_ITEM Carry[64];
+	unsigned int LearnedSkill[2];
+	short ScoreBonus;
+	short SpecialBonus;
+	short SkillBonus;
+	char Critical;
+	char SaveMana;
+	char ShortSkill[4];
+	char GuildLevel;
+	char Magic;
+	char RegenHP;
+	char RegenMP;
+	char Resist[4];
+	char dummy[212];
+	unsigned short CurrentKill;
+	unsigned short TotalKill;
+};
+
+struct STRUCT_AFFECT
+{
+	char Type;
+	char Level;
+	short Value;
+	int Time;
+};
+
+struct STRUCT_MYBONUSEFFECT
+{
+	char cEffect;
+	char cValue;
+	short sValue;
+};
+
+struct STRUCT_MYITEM
+{
+	short sIndex;
+	STRUCT_MYBONUSEFFECT stEffect[3];
+};
+
+struct STRUCT_NEEDITEM
+{
+	int Access;
+	int dINDEX;
+};
+
+struct STRUCT_POSITION
+{
+	int X;
+	int Y;
+};
+
+struct STRUCT_RESULT_ITEMLIST
+{
+	unsigned int dNPCHead;
+	STRUCT_POSITION pxy;
+	STRUCT_MYITEM stItemInfor;
+	unsigned short sMSG;
+	STRUCT_NEEDITEM dNeedItemList[8];
+	unsigned int dCost;
+	char stSameList[20][128];
+};
+
+struct STRUCT_OPTION
+{
+	unsigned int dClass[2];
+	unsigned int dPOS[2];
+};
+
+struct STRUCT_NEED_ITEMLIST
+{
+	int Textnum;
+	unsigned int dVolume;
+	STRUCT_MYITEM stGridItem;
+	unsigned int dListIndexArry[6];
+	STRUCT_OPTION stOption;
+};
+
+struct STRUCT_REFER
+{
+	unsigned int dindex;
+	unsigned int dHavevolume;
+	int bItemListrefer;
+};
+
+struct STRUCT_MIXHELP
+{
+	short Color[9];
+	short Icon;
+	char Help[9][128];
+	char Name[128];
+};
+
+struct STRUCT_MISSIONITEM
+{
+	unsigned int dHavevolume;
+	unsigned int dNeedvolume[2];
+};
+
+struct STRUCT_REPURCHASEITEM
+{
+	int Order;
+	STRUCT_ITEM stItem;
+	int Price;
+};
+
+struct STRUCT_STATICEFFECT
+{
+	short sEffect;
+	short sValue;
+};
+
+struct STRUCT_ITEMLIST
+{
+	char Name[64];
+	short nIndexMesh;
+	short nIndexTexture;
+	short nIndexVisualEffect;
+	short nReqLvl;
+	short nReqStr;
+	short nReqInt;
+	short nReqDex;
+	short nReqCon;
+	STRUCT_STATICEFFECT stEffect[12];
+	int nPrice;
+	short nUnique;
+	short nPos;
+	short nExtra;
+	short nGrade;
+};
+
+struct STRUCT_EXT1
+{
+	int Data[8];
+	STRUCT_AFFECT Affect[32];
+};
+
+struct STRUCT_SUBCLASS
+{
+	unsigned int LearnedSkill[2];
+	STRUCT_ITEM Equip;
+	STRUCT_SCORE CurrentScore;
+	long long Exp;
+	char ShortSkill[20];
+	short ScoreBonus;
+	short SkillBonus;
+};
+
+struct STRUCT_EXT2
+{
+	char Quest[12];
+	unsigned int LastConnectTime;
+	STRUCT_SUBCLASS SubClass[2];
+	char ItemPassWord[16];
+	unsigned int ItemPos;
+	int SendLevItem;
+	short AdminGuildItem;
+	char Dummy[126];
+};
+
+struct STRUCT_DAM
+{
+	unsigned short TargetID;
+	int Damage;
+};
+
+struct STRUCT_SPELL
+{
+	int SkillPoint;
+	int TargetType;
+	int ManaSpent;
+	int Delay;
+	int Range;
+	int InstanceType;
+	int InstanceValue;
+	int TickType;
+	int TickValue;
+	int AffectType;
+	int AffectValue;
+	int AffectTime;
+	char Act1[8];
+	char Act2[8];
+	int InstanceAttribute;
+	int TickAttribute;
+	int Aggressive;
+	int MaxTarget;
+	int bParty;
+	int AffectResist;
+	int Passive;
+	int ForceDamage;
+};
+
+struct STRUCT_INITITEM
+{
+	short PosX;
+	short PosY;
+	short sIndex;
+	short Rotate;
+};
+
+struct STRUCT_AIRMOVELIST
+{
+	int nX;
+	int nY;
+};
+
+struct STRUCT_TOTOLIST
+{
+	char szTime[32];
+	char szTeamA[32];
+	char szTeamB[32];
+};
+
+struct STRUCT_LOTTO
+{
+	short sIndex;
+	char Num[6];
+};
+
+struct STRUCT_SCORE_OLD
+{
+	short Level;
+	short Ac;
+	short Damage;
+	char Reserved;
+	char AttackRun;
+	unsigned short MaxHp;
+	unsigned short MaxMp;
+	unsigned short Hp;
+	unsigned short Mp;
+	short Str;
+	short Int;
+	short Dex;
+	short Con;
+	char Special[4];
+};
+
+struct STRUCT_M_CHECK
+{
+	int Type;
+	int Parm1;
+	int Parm2;
+};
+
+struct STRUCT_GUILDLIST
+{
+	int GuildIndex;
+	char GuildName[4][12];
+	char GCount;
+	char Citizen;
+	char Mandle;
+	unsigned int Fame;
+};
+
+struct STRUCT_TREASURE
+{
+	short Source;
+	STRUCT_ITEM Target[5];
+	short Rate[5];
+};
+
+struct STRUCT_TOTODATA
+{
+	int index;
+	int A_Score;
+	int B_Score;
+	unsigned int A_BuyCount;
+	unsigned int B_BuyCount;
+	unsigned int C_BuyCount;
+	int towshare;
+};
+
+struct STRUCT_ACCOUNT
+{
+	char AccountName[16];
+	char AccountPass[16];
+	char RealName[24];
+	unsigned int SSN1;
+	unsigned int SSN2;
+	char Temp[102];
+	STRUCT_AFFECT Affect[2];
+	unsigned int AccountIndex;
+	unsigned int AccountLastConnectTime;
+	char ItemPassWord[16];
+	unsigned short GameServer;
+	unsigned short Rsv;
+};
+
+struct STRUCT_ACCOUNTFILE
+{
+	STRUCT_ACCOUNT Account;
+	STRUCT_MOB Char[4];
+	STRUCT_ITEM Cargo[128];
+	int Coin;
+	char ShortSkill[4][16];
+	STRUCT_EXT1 Ext1[4];
+	STRUCT_EXT2 Ext2[4];
+};
+
+struct STRUCT_SUBCLASS_OLD
+{
+	unsigned int LearnedSkill;
+	STRUCT_ITEM Equip;
+	STRUCT_SCORE_OLD CurrentScore;
+	unsigned int Exp;
+	char ShortSkill[20];
+	short ScoreBonus;
+	short SkillBonus;
+};
+
+struct STRUCT_EXT2_OLD
+{
+	char Quest[12];
+	unsigned int LastConnectTime;
+	STRUCT_SUBCLASS_OLD SubClass[2];
+	char ItemPassWord[16];
+	unsigned int ItemPos;
+	int SendLevItem;
+	char dummy[112];
+};
+
+struct STRUCT_REQ
+{
+	bool Class;
+	bool Level;
+	bool Str;
+	bool Dex;
+	bool Int;
+	bool Con;
+};
+
+struct STRUCT_LEVSENDITEM
+{
+	int lev;
+	STRUCT_ITEM item[4][2];
+};
+
+struct STRUCT_RANDOMQUIZ
+{
+	char Question[128];
+	char Answer[4][32];
+};
+
+struct STRUCT_ITEMHELP
+{
+	short Color[9];
+	char Help[9][128];
+};
+
+struct STRUCT_MOB_OLD
+{
+	char MobName[16];
+	char Clan;
+	char Merchant;
+	unsigned short Guild;
+	char Class;
+	char Rsv;
+	unsigned short Quest;
+	int Coin;
+	unsigned int Exp;
+	unsigned short HomeTownX;
+	unsigned short HomeTownY;
+	STRUCT_SCORE_OLD BaseScore;
+	STRUCT_SCORE_OLD CurrentScore;
+	STRUCT_ITEM Equip[16];
+	STRUCT_ITEM Carry[64];
+	unsigned int LearnedSkill;
+	short ScoreBonus;
+	short SpecialBonus;
+	short SkillBonus;
+	char Critical;
+	char SaveMana;
+	char ShortSkill[4];
+	char GuildLevel;
+	char Magic;
+	char RegenHP;
+	char RegenMP;
+	char Resist[4];
+};
+
+struct STRUCT_COMBINE
+{
+	int Target;
+	int Source[8];
+	int Refine[8];
+};
+
+struct STRUCT_BEASTBONUS
+{
+	int Int_Damage;
+	int Spe_Damage;
+	int Int_Ac;
+	int Spe_Ac;
+	int Int_Hp;
+	int Spe_Hp;
+	int Int_Run;
+	int Spe_Run;
+	int Int_Attack;
+	int Spe_Attack;
+	int Int_Critical;
+	int Spe_Critical;
+	int Int_Resist;
+	int Spe_Regist;
+	int Int_Delay;
+	int Spe_Delay;
+	int SancLevel;
+};
+
+struct STRUCT_EXT
+{
+	int Data[8];
+	STRUCT_AFFECT Affect[12];
+	char Quest[32];
+};
+
+struct STRUCT_ACCOUNTFILE_OLD
+{
+	STRUCT_ACCOUNT Account;
+	STRUCT_MOB_OLD Char[4];
+	STRUCT_ITEM Cargo[128];
+	int Coin;
+	char ShortSkill[4][16];
+	STRUCT_EXT Ext[4];
+};
+
+struct STRUCT_EXT1_OLD
+{
+	int Data[8];
+	STRUCT_AFFECT Affect[16];
+};
+
+struct STRUCT_MISSION
+{
+	int Arrival;
+	STRUCT_ITEM cItem[8];
+	STRUCT_ITEM rItem[10];
+	STRUCT_M_CHECK Item[8][10];
+	STRUCT_M_CHECK Condition[10];
+	STRUCT_M_CHECK Reward[10];
+};
+
+struct STRUCT_ACCOUNTFILE_OLD2
+{
+	STRUCT_ACCOUNT Account;
+	STRUCT_MOB_OLD Char[4];
+	STRUCT_ITEM Cargo[128];
+	int Coin;
+	char ShortSkill[4][16];
+	STRUCT_EXT1_OLD Ext1[4];
+	STRUCT_EXT2_OLD Ext2[4];
+};
+
+struct STRUCT_GUILDZONE
+{
+	int ChargeGuild;
+	int ChallangeGuild;
+	int GuildStartX;
+	int GuildStartY;
+	STRUCT_POSITION Start;
+	int vx1;
+	int vy1;
+	int vx2;
+	int vy2;
+	int ax1;
+	int ay1;
+	int ax2;
+	int ay2;
+	int hx;
+	int hy;
+	int gx;
+	int gy;
+	int Tax;
+	int ChargeClan;
+	int ChargeCount;
+};
+
+struct STRUCT_SELCHAR_OLD
+{
+	unsigned short HomeTownX[4];
+	unsigned short HomeTownY[4];
+	char MobName[4][16];
+	STRUCT_SCORE_OLD Score[4];
+	STRUCT_ITEM Equip[4][16];
+	unsigned short Guild[4];
+	int Coin[4];
+	unsigned int Exp[4];
+};
+
+struct STRUCT_ACCOUNT_NEW
+{
+	char AccountName[16];
+	char AccountPass[16];
+	char RealName[24];
+	unsigned int SSN1;
+	unsigned int SSN2;
+	char Temp[102];
+	STRUCT_AFFECT Affect[2];
+	unsigned int AccountIndex;
+	unsigned int AccountLastConnectTime;
+	char ItemPassWord[16];
+	unsigned short GameServer;
+	unsigned short Rsv;
+};
+
+struct STRUCT_ACCOUNTFILE_OLD_NEW
+{
+	STRUCT_ACCOUNT_NEW Account;
+	STRUCT_MOB_OLD Char[4];
+	STRUCT_ITEM Cargo[128];
+	int Coin;
+	char ShortSkill[4][16];
+	STRUCT_EXT Ext[4];
+};
+
+struct STRUCT_SAME
+{
+	short dINDEX[8];
+	unsigned int dAttribute;
+};
+
+struct STRUCT_RARE
+{
+	int MonsterFace;
+	int DropRate;
+	STRUCT_ITEM item;
+	int Count;
+};
+
+struct STRUCT_ADMINGUILDITEM
+{
+	int lev;
+	int kind;
+	STRUCT_ITEM item;
+};
+
+struct STRUCT_ACCOUNTFILE_NEW
+{
+	STRUCT_ACCOUNT_NEW Account;
+	STRUCT_MOB Char[4];
+	STRUCT_ITEM Cargo[128];
+	int Coin;
+	char ShortSkill[4][16];
+	STRUCT_EXT1 Ext1[4];
+	STRUCT_EXT2 Ext2[4];
+};
+
+struct STRUCT_ACCOUNTFILE_OLD2_NEW
+{
+	STRUCT_ACCOUNT_NEW Account;
+	STRUCT_MOB_OLD Char[4];
+	STRUCT_ITEM Cargo[128];
+	int Coin;
+	char ShortSkill[4][16];
+	STRUCT_EXT1_OLD Ext1[4];
+	STRUCT_EXT2_OLD Ext2[4];
+};
+
+struct STRUCT_RUNEQUESTZONE
+{
+	int StartX;
+	int StartY;
+	int iLeader;
+	int Type;
+};
+
+struct STRUCT_AUTOKICK
+{
+	char route[4][128];
+};
+
 extern HWND hWndMain;
 extern char EncodeByte[4];
