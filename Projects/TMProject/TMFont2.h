85435c052306eec20750e843be91b775bdcd3062
Kevin Kouketsu <kevinkouketsu53@gmail.com>
2020-07-15 17:25:13 +0200

    TMFont Update

--- Projects/TMProject/TMFont2.h
+++ Projects/TMProject/TMFont2.h
@@ -1,1 +1,25 @@
 #pragma once
+
+class TMFont2
+{
+public:
+    TMFont2();
+    virtual ~TMFont2();
+    
+    int SetText(char* szString, unsigned int dwColor, int bCheckZero);
+    char* GetText();
+    int Render(int nPosX, int nPosY, int nRenderType);
+    int StrByteCheck(char* szString);
+
+public:
+    unsigned int m_dwShadeColor;
+    unsigned int m_dwColor;
+    int m_bMultiLine;
+    float m_fSize;
+    int m_nLineNumber;
+    char m_szString[256];
+    char m_szStringArray[3][44];
+    IDirect3DTexture9* m_pTexture;
+    int m_nPosX;
+    int m_nPosY;
+};
--- Projects/TMProject/TMFont3.h
+++ Projects/TMProject/TMFont3.h
@@ -1,1 +1,43 @@
 #pragma once
+
+#include "TreeNode.h"
+#include "TMFont2.h"
+
+struct stNum
+{
+    int nX;
+    int nY;
+    int nTexIndex;
+};
+
+class IVector2;
+
+class TMFont3 : public TreeNode
+{
+public:
+    TMFont3(char* szText, int nStartX, int nStartY, unsigned int dwColor, float fSize, unsigned int dwDelay, __int16 sDir, unsigned int dwLifeTime, int bViewHalf, int nType);
+    ~TMFont3();
+    
+    int Render();
+    int FrameMove(unsigned int dwServerTime);
+
+public:
+    int m_nStrLen;
+    stNum m_stNum[11];
+    int m_nType;
+    int m_nTextureSetIndex;
+    __int16 m_sDir;
+    IVector2 m_vecPosition;
+    IVector2 m_vecStartPosition;
+    unsigned int m_dwCreateTime;
+    unsigned int m_dwLifeTime;
+    float m_fVisualProgress;
+    float m_fScale;
+    TMFont2 m_Font2;
+    unsigned int m_dwDelay;
+    int m_bVisible;
+    int m_bViewHalf;
+    int m_nSpeed;
+    float m_fCurrScale;
+    float m_fCurrSpeed;
+};
