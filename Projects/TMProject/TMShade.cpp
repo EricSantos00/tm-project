86283de8c8985ac3f9ae00a20f2bb2485e20e352
ericsantosdc <eric_santos2009@hotmail.com>
2020-07-15 13:54:04 -0300

    TMShade members and methods

---
+++ Projects/TMProject/TMShade.cpp
@@ -0,0 +1,3 @@
+#include "pch.h"
+#include "TMEffectBillBoard3.h"
+#include "TMShade.h"
--- Projects/TMProject/TMShade.h
+++ Projects/TMProject/TMShade.h
@@ -1,1 +1,36 @@
 #pragma once
+
+#include "TMEffect.h"
+#include "Structures.h"
+
+class TMShade : public TMEffect
+{
+public:
+	TMShade(int nGridNum, int nTextureIndex, float fScale);
+	~TMShade();
+
+	virtual int SetPosition(TMVector2 vecPosition);
+	int Render() override;
+	virtual void RenderUnder();
+	virtual void SetColor(unsigned int dwColor);
+	int FrameMove(unsigned int dwServerTime) override;
+	int IsVisible() override;
+
+public:
+	int m_nTextureIndex;
+	RDLVERTEX* m_pVertex;
+	unsigned short* m_wpIndex;
+	int m_nVertexNum;
+	int m_nIndexNum;
+	int m_nGridNum;
+	float m_fAngle;
+	unsigned int m_dwCreateTime;
+	unsigned int m_dwLifeTime;
+	unsigned int m_dwA;
+	unsigned int m_dwR;
+	unsigned int m_dwG;
+	unsigned int m_dwB;
+	int m_bFI;
+	int m_nFade;
+	float m_fScale;
+};
