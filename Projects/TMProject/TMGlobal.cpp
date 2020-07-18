5825fd12614d8b43bc0a22956a4523f340f5d4b7
Kevin Kouketsu <kevinkouketsu53@gmail.com>
2020-07-18 15:55:58 -0300

    Added TMGlobal with global vars

---
+++ Projects/TMProject/TMGlobal.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+#include "TMGlobal.h"
+
+NewApp* g_pApp;
+ObjectManager* g_pObjectManager;
+EventTranslator* g_pEventTranslator;
+MeshManager* g_pMeshManager;
+TextureManager* g_pTextureManager;
+TimerManager* g_pTimerManager;
+TMScene* g_pCurrentScene;
+RenderDevice* g_pDevice;
---
+++ Projects/TMProject/TMGlobal.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "NewApp.h"
+#include "CPSock.h"
+#include "ObjectManager.h"
+#include "MeshManager.h"
+#include "TextureManager.h"
+#include "TimerManager.h"
+#include "TMScene.h"
+#include "RenderDevice.h"
+
+extern NewApp* g_pApp;
+extern CPSock* g_pSocketManager;
+extern CPSock* g_LoginSocket;
+extern ObjectManager* g_pObjectManager;
+extern EventTranslator* g_pEventTranslator;
+extern MeshManager* g_pMeshManager;
+extern TextureManager* g_pTextureManager;
+extern TimerManager* g_pTimerManager;
+extern TMScene* g_pCurrentScene;
+extern RenderDevice* g_pDevice;
