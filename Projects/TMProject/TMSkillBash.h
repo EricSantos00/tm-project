de41dc5d8736c1632313473ae03de72294e48906
EricSantos00 <eric_santos2009@hotmail.com>
2020-07-21 13:04:43 -0300

    TMSkillBash, TMSkillCure, TMSkillDoubleSwing member and functions

--- Projects/TMProject/TMSkillBash.h
+++ Projects/TMProject/TMSkillBash.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "TMEffect.h"
+
+class TMSkillBash : public TMEffect
+{
+public:
+	TMSkillBash(TMVector3 vecPosition, int nType);
+	~TMSkillBash();
+
+	int FrameMove(unsigned int dwServerTime) override;
+
+public:
+	unsigned int m_dwStartTime;
+	unsigned int m_dwLifeTime;
+	int m_nType;
+	unsigned int m_dwLastTime;
+};
--- Projects/TMProject/TMSkillDoubleSwing.h
+++ Projects/TMProject/TMSkillDoubleSwing.h
@@ -1,1 +1,33 @@
 #pragma once
+
+#include "TMEffect.h"
+#include "Structures.h"
+
+class TMShade;
+class TMEffectBillBoard;
+class TMSkillDoubleSwing : public TMEffect
+{
+public:
+    TMSkillDoubleSwing(TMVector3 vecStart, TMVector3 vecTarget, int nLevel, TMObject* pOwner);
+    ~TMSkillDoubleSwing();
+
+    int Render() override;
+    int IsVisible() override;
+    int FrameMove(unsigned int dwServerTime) override;
+
+public:
+    unsigned int m_dwStartTime;
+    TMVector3 m_vecStartPos;
+    TMVector3 m_vecTargetPos;
+    int m_nLevel;
+    int m_nMeshIndex;
+    float m_fAngle;
+    float m_fAngle2;
+    float m_fProgress;
+    unsigned int m_dwLifeTime;
+    unsigned int m_dwOldTime;
+    TMShade* m_pLightMap;
+    TMEffectBillBoard* m_pCenterLight;
+    TMEffectBillBoard* m_pCenterLight2;
+    TMEffectBillBoard* m_pCenterFlare;
+};
