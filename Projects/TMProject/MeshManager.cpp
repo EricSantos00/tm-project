e522951e2f89d5957a1b29cc13d44424bdb1ac34
EricSantos00 <eric_santos2009@hotmail.com>
2020-07-21 09:18:00 -0300

    Added static member class initialization

--- Projects/TMProject/MeshManager.cpp
+++ Projects/TMProject/MeshManager.cpp
@@ -1,3 +1,8 @@
 #include "pch.h"
 #include "TMMesh.h"
 #include "MeshManager.h"
+
+stBoneAni MeshManager::m_BoneAnimationList[100]{};
+short MeshManager::m_sAnimationArray[6][60][56]{};
+char MeshManager::m_aObjectMask[2048][16][16]{};
+int MeshManager::m_nFullCount{0};
--- Projects/TMProject/TextureManager.cpp
+++ Projects/TMProject/TextureManager.cpp
@@ -1,2 +1,5 @@
 #include "pch.h"
 #include "TextureManager.h"
+
+int TextureManager::DYNAMIC_TEXTURE_WIDTH = 256;
+int TextureManager::DYNAMIC_TEXTURE_HEIGHT = 256;
