dd1697598ca8ee2e8255592ba87c6d40d1c3e132
Kevin Kouketsu <kevinkouketsu53@gmail.com>
2020-07-23 19:21:34 -0300

    SetFocusedControl and AddItem implemented

--- Projects/TMProject/SControlContainer.cpp
+++ Projects/TMProject/SControlContainer.cpp
@@ -1,4 +1,5 @@
 #include "pch.h"
+#include "TMFieldScene.h"
 #include "SControlContainer.h"
 #include "SControl.h"
 #include "TMGlobal.h"
@@ -68,6 +69,50 @@ int SControlContainer::OnIMEEvent(char* ipComposeString)
 
 void SControlContainer::SetFocusedControl(SControl* pControl)
 {
+	if (g_nKeyType != 1 || pControl && pControl->m_eCtrlType == CONTROL_TYPE::CTRL_TYPE_EDITABLETEXT)
+	{
+		if (m_pFocusControl)
+			m_pFocusControl->SetFocused(0);
+
+		m_pFocusControl = pControl;
+
+		if(m_pFocusControl)
+			m_pFocusControl->SetFocused(1);
+
+		TMScene* pScene = g_pCurrentScene;
+		if (g_pCurrentScene)
+		{
+			if (m_pFocusControl != nullptr && m_pFocusControl->m_eCtrlType != CONTROL_TYPE::CTRL_TYPE_EDITABLETEXT)
+			{
+				g_pCurrentScene->m_pAlphaNative->SetVisible(1);
+				g_pEventTranslator->UpdateCompositionPos();
+
+				SPanel* panel = static_cast<SPanel*>(g_pCurrentScene->m_pControlContainer->FindControl(65670u));
+				if (panel && panel->m_bVisible && pScene->m_eSceneType == ESCENE_TYPE::ESCENE_FIELD)
+					static_cast<TMFieldScene*>(pScene)->m_pChatSelectPanel->SetVisible(1);
+			}
+			else
+			{
+				g_pCurrentScene->m_pAlphaNative->SetVisible(0);
+
+				if (pScene->m_eSceneType == ESCENE_TYPE::ESCENE_FIELD)
+				{
+					static_cast<TMFieldScene*>(pScene)->m_pChatSelectPanel->SetVisible(1);
+					static_cast<TMFieldScene*>(pScene)->m_pChatListPanel->SetVisible(1);
+				}
+
+				pScene->m_pTextIMEDesc->SetVisible(0);
+			}
+
+			if (m_pFocusControl && m_pFocusControl->IsIMENative())
+			{
+				if (g_pEventTranslator)
+					g_pEventTranslator->SetIMENative();
+			}
+			else if (g_pEventTranslator)
+				g_pEventTranslator->SetIMEAlphaNumeric();
+		}
+	}
 }
 
 int SControlContainer::OnControlEvent(DWORD idwControlID, DWORD idwEvent)
@@ -77,6 +122,7 @@ int SControlContainer::OnControlEvent(DWORD idwControlID, DWORD idwEvent)
 
 void SControlContainer::AddItem(SControl* pControl)
 {
+	m_pControlRoot->AddChild(pControl);
 }
 
 int SControlContainer::FrameMove(unsigned int dwServerTime)
@@ -123,7 +169,8 @@ SControl* SControlContainer::FindControl(unsigned int dwID)
 
 void SControlContainer::GenerateText(const char* pFileName)
 {
-	FILE* fp = nullptr;
+	// Skip that function for now... just create a text file with content on screen
+	/*FILE* fp = nullptr;
 	fopen_s(&fp, pFileName, "wt");
 
 	if (fp)
@@ -141,7 +188,10 @@ void SControlContainer::GenerateText(const char* pFileName)
 					{
 						for (int i = 0 < MAX_RESOURCE_LIST; ++i)
 						{
-
+							if (g_pObjectManager->m_ResourceList[i].nNumber == pCurrentControl->m_dwControlID)
+							{
+								const char* text = TMFont2 pCurrentControl->
+							}
 						}
 					}
 				}
@@ -151,5 +201,5 @@ void SControlContainer::GenerateText(const char* pFileName)
 		}
 
 		fclose(fp);
-	}
+	}*/
 }
