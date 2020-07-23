64d565f8bb4cef79b9415a1067465ea1d09625f8
FREEDOM <56805685+wsphantom@users.noreply.github.com>
2020-07-23 01:06:53 -0300

    Updated TreeNode class methods

--- Projects/TMProject/TreeNode.cpp
+++ Projects/TMProject/TreeNode.cpp
@@ -14,10 +14,46 @@ int TreeNode::m_VisualKey9 = -1;
 
 TreeNode::TreeNode(unsigned int id)
 {
+	m_pTop = nullptr;
+	m_pPrevLink = nullptr;
+	m_pNextLink = nullptr;
+	m_pDown = nullptr;
+	m_cDeleted = 0;
+	m_dwKey = -1;
+	m_pOwner = nullptr;
+	m_dwID = id;
 }
 
 TreeNode::~TreeNode()
 {
+	TreeNode* pNext;
+
+	for (TreeNode* pCurrent = m_pDown; pCurrent; pCurrent = pNext)
+	{
+		pNext = pCurrent->m_pNextLink;
+
+		if (pCurrent)
+			delete pCurrent;
+	}
+
+	if (m_pPrevLink)
+		m_pPrevLink->m_pNextLink = m_pNextLink;
+
+	if (m_pNextLink)
+		m_pNextLink->m_pPrevLink = m_pPrevLink;
+
+	if (m_pTop)
+	{
+		if (m_pTop->m_pDown == this)
+		{
+			m_pTop->m_pDown = m_pNextLink;
+
+			if (m_pNextLink)
+				m_pNextLink->m_pTop = m_pTop;
+		}
+
+		m_pTop = nullptr;
+	}
 }
 
 int TreeNode::OnPacketEvent(unsigned int dwCode, char* buf)
@@ -83,37 +119,106 @@ void TreeNode::InitTexture()
 
 void TreeNode::AddChild(TreeNode* pChild)
 {
+	pChild->m_pTop = this;
+	pChild->m_pNextLink = m_pDown;
+
+	m_pDown = pChild;
+
+	if (pChild->m_pNextLink)
+		pChild->m_pNextLink->m_pPrevLink = pChild;
 }
 
 void TreeNode::AddChild(TreeNode* pChild, unsigned int id)
 {
+	pChild->m_pTop = this;
+	pChild->m_pNextLink = m_pDown;
+
+	m_pDown = pChild;
+
+	if (pChild->m_pNextLink)
+		pChild->m_pNextLink->m_pPrevLink = pChild;
+
+	pChild->m_dwID = id;
 }
 
 void TreeNode::AddChild(TreeNode* pChild, unsigned int id, unsigned int key)
 {
+	pChild->m_pTop = this;
+	pChild->m_pNextLink = m_pDown;
+
+	m_pDown = pChild;
+
+	if (pChild->m_pNextLink)
+		pChild->m_pNextLink->m_pPrevLink = pChild;
+
+	pChild->m_dwID = id;
+	pChild->m_dwKey = key;
 }
 
 void TreeNode::SetPrevNode(TreeNode* pPrev)
 {
+	pPrev->m_pTop = m_pTop;
+	pPrev->m_pNextLink = this;
+
+	m_pPrevLink->m_pNextLink = pPrev;
+
+	pPrev->m_pPrevLink = m_pPrevLink;
+
+	m_pPrevLink = pPrev;
+
+	if (m_pTop->m_pDown == this)
+		m_pTop->m_pDown = pPrev;
 }
 
 void TreeNode::SetNextNode(TreeNode* pNext)
 {
+	pNext->m_pTop = m_pTop;
+
+	pNext->m_pPrevLink = this;
+	pNext->m_pNextLink = m_pNextLink;
+
+	m_pNextLink = pNext;
 }
 
 void TreeNode::SetDeleted()
 {
+	m_cDeleted = 1;
+
+	if (m_pDown)
+		m_pDown->SetDeleted();
 }
 
 void TreeNode::AddChildWithKey(TreeNode* node, unsigned int Key)
 {
+	TreeNode* n = FindNodeWithKey(Key);
+
+	if (n)
+		n->AddChild(node, 0, Key);
+	else
+		AddChild(node, 0, Key);
 }
 
 TreeNode* TreeNode::FindNodeWithKey(unsigned int Key)
 {
-	return nullptr;
+	TreeNode* n;
+
+	for (n = m_pDown; n && n->m_dwKey != Key; n = n->m_pNextLink);
+
+	return n;
 }
 
 void TreeNode::DeleteOwner(TMObject* pOwner)
 {
+	TreeNode* n;
+
+	for (n = m_pDown; n; n = n->m_pNextLink)
+	{
+		if (pOwner)
+		{
+			if (n->m_pOwner == pOwner)
+				n->m_pOwner = nullptr;
+		}
+		else
+			n->m_pOwner = nullptr;
+	}
 }
