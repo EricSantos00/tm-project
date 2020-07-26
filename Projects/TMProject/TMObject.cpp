2639a693f8f024cde8cfcd440f061752de5f36b8
EricSantos00 <eric_santos2009@hotmail.com>
2020-07-26 18:54:56 -0300

    TMObject::isCamPos descompiled

--- Projects/TMProject/TMObject.cpp
+++ Projects/TMProject/TMObject.cpp
@@ -5,6 +5,7 @@
 #include "Basedef.h"
 #include "TMLog.h"
 #include "TMCamera.h"
+#include "TMMesh.h"
 
 TMObject::TMObject()
 	: TreeNode(0)
@@ -221,7 +222,87 @@ int TMObject::isVisualKey()
 
 int TMObject::isCamPos()
 {
-	return 0;
+	TMMesh *pMesh = g_pMeshManager->GetCommonMesh(m_dwObjType, 0, 180000);
+	if (!pMesh)
+		return 0;
+
+	if (g_pCurrentScene->GroundGetMask(m_vecPosition) < 126)
+		return 0;
+
+	TMCamera* pCamera = g_pObjectManager->m_pCamera;
+
+	D3DXVECTOR3 VecCam = D3DXVECTOR3(pCamera->m_cameraPos.x, pCamera->m_cameraPos.z, pCamera->m_cameraPos.y);
+	D3DXVECTOR3 VecCenter = D3DXVECTOR3(pCamera->m_pFocusedObject->m_vecPosition.x, pCamera->m_pFocusedObject->m_vecPosition.y, pCamera->m_pFocusedObject->m_fHeight);
+	D3DXVECTOR3 Vec = D3DXVECTOR3(m_vecPosition.x, m_vecPosition.y, m_fHeight) - VecCenter;
+
+	if (D3DXVec3Dot(&Vec, &D3DXVECTOR3(pCamera->m_vecCamDir.x, pCamera->m_vecCamDir.z, pCamera->m_vecCamDir.y)) > 0.0f)
+		return 0;
+
+	D3DXVECTOR3 CamPos(pCamera->m_cameraPos.x, pCamera->m_cameraPos.y, pCamera->m_cameraPos.z);
+	D3DXVECTOR3 CamDir(pCamera->m_vecCamDir.x, pCamera->m_vecCamDir.y, pCamera->m_vecCamDir.z);
+
+	D3DXVECTOR3 vecPos[9];
+	for (int i = 0; i < 9; ++i)
+	{
+		vecPos[i].x = m_vecPosition.x;
+		vecPos[i].y = m_vecPosition.y;
+		vecPos[i].z = m_fHeight;
+	}
+
+	vecPos[1].x = vecPos[3].x = vecPos[5].x = vecPos[7].x = vecPos[7].x + pMesh->m_fMinX;
+	vecPos[1].z = vecPos[2].z = vecPos[5].z = vecPos[6].z = vecPos[6].z + pMesh->m_fMinY;
+	vecPos[1].y = vecPos[2].y = vecPos[3].y = vecPos[4].y = vecPos[4].y + pMesh->m_fMinZ;
+
+	vecPos[2].x = vecPos[4].x = vecPos[6].x = vecPos[8].x = vecPos[8].x + pMesh->m_fMaxX;
+	vecPos[3].z = vecPos[4].z = vecPos[7].z = vecPos[8].z = vecPos[8].z + pMesh->m_fMaxY;
+	vecPos[5].y = vecPos[6].y = vecPos[7].y = vecPos[8].y = vecPos[8].y + pMesh->m_fMaxZ;
+
+	D3DXMATRIX matScale;
+	D3DXMATRIX matPosition;
+	D3DXMATRIX mat;
+	D3DXMATRIX mat2;
+	D3DXMatrixScaling(&matScale,pMesh->m_fScaleH, pMesh->m_fScaleV, pMesh->m_fScaleH);
+	D3DXMatrixRotationYawPitchRoll(&mat, m_fAngle, D3DXToRadian(90), 0);
+	D3DXMatrixMultiply(&mat, &g_pDevice->m_matWorld, &mat);
+	D3DXMatrixMultiply(&mat, &mat, &matScale);
+
+	for (int i = 0; i < 9; ++i)
+	{
+		D3DXMatrixTranslation(&matPosition, vecPos[i].x, vecPos[i].y, vecPos[i].z);
+		D3DXMatrixMultiply(&mat2, &mat, &matPosition);
+		vecPos[i].x = mat2._41;
+		vecPos[i].y = mat2._42;
+		vecPos[i].z = mat2._43;
+	}
+
+	int nAlpha = 0;
+	if (D3DXIntersectTri(&vecPos[1], &vecPos[2], &vecPos[5], &CamPos, &CamDir, 0, 0, 0) == 1)
+		nAlpha = 1;
+	else if (D3DXIntersectTri(&vecPos[5], &vecPos[6], &vecPos[1], &CamPos, &CamDir, 0, 0, 0) == 1)
+		nAlpha = 1;
+	else if (D3DXIntersectTri(&vecPos[1], &vecPos[4], &vecPos[6], &CamPos, &CamDir, 0, 0, 0) == 1)
+		nAlpha = 1;
+	else if (D3DXIntersectTri(&vecPos[6], &vecPos[8], &vecPos[4], &CamPos, &CamDir, 0, 0, 0) == 1)
+		nAlpha = 1;
+	else if (D3DXIntersectTri(&vecPos[3], &vecPos[4], &vecPos[7], &CamPos, &CamDir, 0, 0, 0) == 1)
+		nAlpha = 1;
+	else if (D3DXIntersectTri(&vecPos[7], &vecPos[8], &vecPos[4], &CamPos, &CamDir, 0, 0, 0) == 1)
+		nAlpha = 1;
+	else if (D3DXIntersectTri(&vecPos[1], &vecPos[3], &vecPos[5], &CamPos, &CamDir, 0, 0, 0) == 1)
+		nAlpha = 1;
+	else if(D3DXIntersectTri(&vecPos[5], &vecPos[7], &vecPos[3], &CamPos, &CamDir, 0, 0, 0) == 1)
+		nAlpha = 1;
+
+	float fRadius = 1.0f;
+	fRadius = pMesh->m_fRadius;
+	if (nAlpha == 1)
+	{
+		TMVector2 Position(CamPos.x, CamPos.z);
+		if (g_pCurrentScene->GroundGetMask(Position) > 125)
+			nAlpha = 2;
+	}
+
+	return nAlpha;
 }
 
 void TMObject::ResetObject()
