50a326d70f795b73d38610b1dddd5ecc804d8ebd
EricSantos00 <eric_santos2009@hotmail.com>
2020-07-27 10:58:15 -0300

    TMSky::FrameMove started

--- Projects/TMProject/TMSky.cpp
+++ Projects/TMProject/TMSky.cpp
@@ -3,6 +3,9 @@
 #include "TMSky.h"
 #include "TMGlobal.h"
 #include "TMCamera.h"
+#include "MeshManager.h"
+#include "TMSun.h"
+#include "TMObject.h"
 
 // NOTE: this is cleary not float values.
 D3DCOLORVALUE TMSky::m_LightVal[4] =
@@ -214,7 +217,310 @@ int TMSky::Render()
 
 int TMSky::FrameMove(unsigned int dwServerTime)
 {
-	return 0;
+    if (g_bHideBackground)
+    {
+        g_pDevice->m_dwClearColor = 0;
+        return 0;
+    }
+
+   TMMesh* pMesh = g_pMeshManager->GetCommonMesh(m_dwObjType, 1, 180000);
+    if (g_pCurrentScene->m_pSun != nullptr)
+    {
+        g_pCurrentScene->m_pSun->m_bHide = m_nState && m_nState < 10 ? 1 : 0;
+        if (m_nTextureIndex == 68 || pMesh->m_nTextureIndex[0] == 68)
+            g_pCurrentScene->m_pSun->m_bHide = 1;
+    } 
+    
+    if (pMesh == nullptr)
+        return 0;
+
+    if (pMesh->m_pVB == nullptr)
+        return 0;
+
+    D3DVERTEXBUFFER_DESC vDesc;
+    RDLVERTEX* pVertex;
+
+    pMesh->m_pVB->GetDesc(&vDesc);
+    pMesh->m_pVB->Lock(0, 0, (void**)&pVertex, 0);
+
+    g_ClipFar = 70.0f;
+    unsigned int dwAlpha = 0x00808080;
+
+    if (RenderDevice::m_bDungeon == 1)
+    {
+        TMVector2 vec(0.0f, 0.0f);
+
+        // NOTE: this code is never reached, they are made and removed for some reason
+        if ((int)vec.x >> 7 > 16 && (int)vec.x >> 7 < 20 && (int)vec.y >> 7 > 29)
+        {
+            g_pDevice->m_colorLight.r = 1.0f;
+            g_pDevice->m_colorLight.g = 0.8f;
+            g_pDevice->m_colorLight.b = 0.8f;
+            if ((int)vec.x >> 7 != 18 || (int)vec.y >> 7 != 30)
+            {
+                g_pDevice->m_fFogStart = TMSky::FogList[0][0];
+                g_pDevice->m_fFogEnd = TMSky::FogList[0][1];
+            }
+            else
+            {
+                g_pDevice->m_fFogStart = TMSky::FogList[1][0];
+                g_pDevice->m_fFogEnd = TMSky::FogList[1][1];
+            }
+            g_pDevice->m_colorBackLight.r = 0.3f;
+            g_pDevice->m_colorBackLight.g = 0.0;
+            g_pDevice->m_colorBackLight.b = 0.0;
+            g_pDevice->m_colorLight.a = 1.0f;
+            g_pDevice->m_dwClearColor = 0x160000;
+        }
+        else if ((int)vec.x >> 7 == 31 && (int)vec.y >> 7 == 31)
+        {
+            g_pDevice->m_colorLight.r = 1.0f;
+            g_pDevice->m_colorLight.g = 1.0f;
+            g_pDevice->m_colorLight.b = 1.0f;
+            g_pDevice->m_fFogStart = TMSky::FogList[2][0];
+            g_pDevice->m_fFogEnd = TMSky::FogList[2][1];
+            g_pDevice->m_colorBackLight.r = 0.6f;
+            g_pDevice->m_colorBackLight.g = 0.6f;
+            g_pDevice->m_colorBackLight.b = 0.6f;
+            g_pDevice->m_colorLight.a = 1.0f;
+        }
+        else if ((int)vec.x >> 7 >= 28 && (int)vec.x >> 7 <= 30 && 
+                 (int)vec.y >> 7 >= 27 && vec.y <= 28)
+        {
+            g_pDevice->m_colorLight.r = 0.89f;
+            g_pDevice->m_colorLight.g = 0.89f;
+            g_pDevice->m_colorLight.b = 0.89f;
+            g_pDevice->m_fFogStart = TMSky::FogList[3][0];
+            g_pDevice->m_fFogEnd = TMSky::FogList[3][1];
+            g_pDevice->m_colorBackLight.r = 0.0f;
+            g_pDevice->m_colorBackLight.g = 0.0f;
+            g_pDevice->m_colorBackLight.b = 0.0f;
+            g_pDevice->m_colorLight.a = 1.0f;
+            g_pDevice->m_dwClearColor = 0;
+        }
+        else
+        {
+            g_pDevice->m_colorLight.r = 1.0f;
+            g_pDevice->m_colorLight.g = 1.0f;
+            g_pDevice->m_colorLight.b = 1.0f;
+            g_pDevice->m_colorLight.a = 1.0f;
+            g_pDevice->m_colorBackLight.r = 0.4f;
+            g_pDevice->m_colorBackLight.g = 0.4f;
+            g_pDevice->m_colorBackLight.b = 0.4f;
+            g_pDevice->m_dwClearColor = 0;
+            g_pDevice->m_fFogStart = TMSky::FogList[4][0];
+            g_pDevice->m_fFogEnd = TMSky::FogList[4][1];
+        }
+
+        if (g_pCurrentScene->m_pSun != nullptr)
+            g_pCurrentScene->m_pSun->m_bHide = 1;
+    }
+    else if (RenderDevice::m_bDungeon == 2)
+    {
+        g_pDevice->m_dwClearColor = 0;
+        g_pDevice->m_fFogStart = TMSky::FogList[5][0];
+        g_pDevice->m_fFogEnd = TMSky::FogList[5][1];
+        g_pCurrentScene->m_pSun->m_bHide = 1;
+
+        TMVector2 vec = g_pObjectManager->m_pCamera->m_pFocusedObject->m_vecPosition;
+
+        if (((int)vec.x >> 7 == 13 || (int)vec.x >> 7 == 14) && 
+            (int)vec.y >> 7 == 28)
+        {
+            g_pDevice->m_colorLight.r = 0.8f;
+            g_pDevice->m_colorLight.g = 0.8f;
+            g_pDevice->m_colorLight.b = 0.8f;
+            g_pDevice->m_fFogStart = TMSky::FogList[6][0];
+            g_pDevice->m_fFogEnd = TMSky::FogList[6][1];
+            g_pDevice->m_colorBackLight.r = 0.6f;
+            g_pDevice->m_colorBackLight.g = 0.6f;
+            g_pDevice->m_colorBackLight.b = 0.6f;
+            g_pDevice->m_colorLight.a = 1.0f;
+        }
+        else
+        {
+            g_pDevice->m_colorLight.r = 1.0f;
+            g_pDevice->m_colorLight.g = 1.0f;
+            g_pDevice->m_colorLight.b = 1.0f;
+            g_pDevice->m_colorLight.a = 1.0f;
+            g_pDevice->m_colorBackLight.r = 0.2f;
+            g_pDevice->m_colorBackLight.g = 0.3f;
+            g_pDevice->m_colorBackLight.b = 0.2f;
+        }
+    }
+    else if (RenderDevice::m_bDungeon == 5)
+    {
+        g_pDevice->m_colorLight.r = 1.0f;
+        g_pDevice->m_colorLight.g = 1.0f;
+        g_pDevice->m_colorLight.b = 1.0f;
+        g_pDevice->m_colorLight.a = 1.0f;
+        g_pDevice->m_colorBackLight.r = 0.4f;
+        g_pDevice->m_colorBackLight.g = 0.4f;
+        g_pDevice->m_colorBackLight.b = 0.4f;
+        g_pDevice->m_dwClearColor = 0;
+        g_pDevice->m_fFogStart = TMSky::FogList[4][0];
+        g_pDevice->m_fFogEnd = TMSky::FogList[4][1];
+    }
+    else
+    {
+        g_pDevice->m_colorBackLight.r = 0.4f;
+        g_pDevice->m_colorBackLight.g = 0.4f;
+        g_pDevice->m_colorBackLight.b = 0.4f;
+        if (g_pCurrentScene->m_eSceneType == ESCENE_TYPE::ESCENE_SELECT_SERVER)
+        {
+            g_pDevice->m_fFogStart = TMSky::FogList[9][0];
+            g_pDevice->m_fFogEnd = TMSky::FogList[9][1];
+        }
+        if (g_pCurrentScene->m_eSceneType == ESCENE_TYPE::ESCENE_DEMO)
+        {
+            g_pDevice->m_fFogStart = TMSky::FogList[10][0];
+            g_pDevice->m_fFogEnd = TMSky::FogList[10][1];
+        }
+        else if (g_pCurrentScene->m_eSceneType == ESCENE_TYPE::ESCENE_SELCHAR)
+        {
+            g_pDevice->m_fFogStart = TMSky::FogList[11][0];
+            g_pDevice->m_fFogEnd = TMSky::FogList[11][1];
+            g_pDevice->m_bFog = 0;
+        }
+        else
+        {
+            TMVector2 vec(0.0f, 0.0f);
+            if (g_pCurrentScene->m_eSceneType == ESCENE_TYPE::ESCENE_FIELD)
+                vec = g_pObjectManager->m_pCamera->m_pFocusedObject->m_vecPosition;
+
+            if ((int)vec.x >> 7 > 26 && (int)vec.x >> 7 < 31 &&
+                (int)vec.y >> 7 > 20 && (int)vec.y >> 7 < 25)
+            {
+                g_pDevice->m_colorLight.r = 1.0f;
+                g_pDevice->m_colorLight.g = 1.0f;
+                g_pDevice->m_colorLight.b = 1.0f;
+                g_pDevice->m_fFogStart = TMSky::FogList[8][0];
+                g_pDevice->m_fFogEnd = TMSky::FogList[8][1];
+                g_pDevice->m_colorBackLight.r = 0.6f;
+                g_pDevice->m_colorBackLight.g = 0.6f;
+                g_pDevice->m_colorBackLight.b = 0.6f;
+                g_pDevice->m_colorLight.a = 1.0f;
+                m_fScale = 0.89f;
+                g_ClipFar = 90.0f;
+                g_pDevice->m_dwClearColor = 0xFFFFFFFF;
+                if (((int)vec.x >> 7 == 29 || (int)vec.x >> 7 == 30) &&
+                    (int)vec.y >> 7 == 22)
+                {
+                    g_pCurrentScene->m_pSun->m_bHide = 1;
+                }
+            }
+            else
+            {
+                g_pDevice->m_fFogStart = TMSky::FogList[7][0];
+                g_pDevice->m_fFogEnd = TMSky::FogList[7][1];
+                m_fScale = 0.5f;
+                if (g_pObjectManager->m_pCamera->m_fVerticalAngle <= -0.5f)
+                {
+                    if ((int)g_pDevice->m_fFogEnd == 24)
+                        g_pDevice->m_fFogEnd = 24.0f;
+                    else
+                        g_pDevice->m_fFogEnd = 24.0f - ((24.0f - g_pDevice->m_fFogEnd) * 0.89f);
+                }
+                else if ((int)g_pDevice->m_fFogEnd == 34)
+                {
+                    g_pDevice->m_fFogEnd = 34.0f;
+                }
+                else
+                {
+                    g_pDevice->m_fFogEnd = 34.0f - ((34.0f - g_pDevice->m_fFogEnd) * 0.89f);
+                }
+            }
+        }
+
+        g_pDevice->m_fFogStart = g_pDevice->m_fFogStart + 5.0f;
+        g_pDevice->m_fFogEnd = g_pDevice->m_fFogEnd + 5.0f;
+        if (m_nState / 10 == 1)
+        {
+            unsigned int dwElapsed = g_pTimerManager->GetServerTime() - m_dwStartTime;
+            if (dwElapsed >= m_dwChangeTime)
+            {
+                SetWeatherState(m_nState % 10);
+                g_pDevice->m_colorLight = m_LightVal[m_nState % 10];
+            }
+            else
+            {
+                float fProgress = (float)dwElapsed / (float)m_dwChangeTime;
+                dwAlpha = 0x888888 | ((unsigned int)(float)(fProgress * 255.0f) << 24);
+                if (m_nState == 13)
+                {
+                    unsigned int dwMoon = 0xFFFFFF | ((unsigned int)(float)(fProgress * 255.0f) << 24);
+                    m_ebMoon[0].SetColor(dwMoon);
+                }
+                else if (m_nState == 10 && m_nTextureIndex == 67)
+                {
+                    unsigned int dwMoon = 0xFFFFFF | ((unsigned int)(float)((1.0f - fProgress) * 255.0f) << 24);
+                    m_ebMoon[0].SetColor(dwMoon);
+
+                    if (g_pCurrentScene->m_pSun != nullptr)
+                        g_pCurrentScene->m_pSun->m_fDefSize = fProgress;
+                }
+                else if (m_nState > 10 && (m_nTextureIndex == 68 || m_nTextureIndex == 69) && g_pCurrentScene->m_pSun != nullptr)
+                    g_pCurrentScene->m_pSun->m_fDefSize = 1.0f - fProgress;
+                
+                // TODO: this code maybe crash. The -264 maybe is - 67.
+                unsigned int dwR = (unsigned char)((float)m_dwR[pMesh->m_nTextureIndex[0] - 264] * (float)(1.0f - fProgress)) +
+                    (float)((float)m_dwR[m_nTextureIndex - 264] * fProgress);
+                unsigned int dwG = (unsigned char)((float)m_dwG[pMesh->m_nTextureIndex[0] - 264] * (float)(1.0f - fProgress)) +
+                    (float)((float)m_dwG[m_nTextureIndex - 264] * fProgress);
+                unsigned int dwB = (unsigned char)((float)m_dwB[pMesh->m_nTextureIndex[0] - 264] * (float)(1.0f - fProgress)) +
+                    (float)((float)m_dwB[m_nTextureIndex - 264] * fProgress);
+
+                g_pDevice->m_dwClearColor = dwB | (dwG << 8) | (dwR << 16);
+
+                g_pDevice->m_colorLight.r = (float)((float)(1.0 - fProgress)
+                    * TMSky::m_LightVal[pMesh->m_nTextureIndex[0] - 67].r)
+                    + (float)(TMSky::m_LightVal[m_nTextureIndex - 67].r * fProgress);
+                g_pDevice->m_colorLight.g = (float)((float)(1.0 - fProgress)
+                    * TMSky::m_LightVal[pMesh->m_nTextureIndex[0] - 67].g)
+                    + (float)(TMSky::m_LightVal[m_nTextureIndex - 67].g * fProgress);
+                g_pDevice->m_colorLight.b = (float)((float)(1.0 - fProgress)
+                    * TMSky::m_LightVal[pMesh->m_nTextureIndex[0] - 67].b)
+                    + (float)(TMSky::m_LightVal[m_nTextureIndex - 67].b * fProgress);
+            }
+        }
+        else
+        {
+            SetWeatherState(m_nState % 10);
+            g_pDevice->m_colorLight = m_LightVal[m_nState % 10];
+        }
+    }
+
+    if (g_pObjectManager->m_pCamera->m_bInWater)
+    {
+        g_pDevice->m_fFogStart = 1.0f;
+        g_pDevice->m_fFogEnd = 10.0f;
+
+        if (m_nState == 0|| RenderDevice::m_bDungeon == 1 || RenderDevice::m_bDungeon == 2)
+            g_pDevice->m_dwClearColor = 0xFF335599;        
+        else if(m_nState == 1)
+            g_pDevice->m_dwClearColor = 0xFF333333;
+        else if(m_nState == 2)
+            g_pDevice->m_dwClearColor = 0xFF441100;
+        else if(m_nState == 3)
+            g_pDevice->m_dwClearColor = 0xFF222222;
+    }
+
+    if (m_nState != 0 && m_nState != 2 && m_nState != 12)
+    {
+        for (int k = 0; k < 2; ++k)
+        {
+            TMObjectContainer* pContainer = g_pCurrentScene->m_pObjectContainerList[k];
+            if (pContainer != nullptr)
+            {
+                for (int l = 0; l < pContainer->m_nLightIndex; ++l)
+                    pContainer->m_pLightContainer[l]->m_bEnable = 1;
+            }
+        }
+    }
+    else
+    {
+
+    }
 }
 
 void TMSky::RestoreDeviceObjects()
