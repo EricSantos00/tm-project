50aaf3b299ebbb8923cffef0fa3044691e7cdd7e
EricSantos00 <eric_santos2009@hotmail.com>
2020-07-28 13:18:01 -0300

    Fix TMGround Render

--- Projects/TMProject/TMUtil.h
+++ Projects/TMProject/TMUtil.h
@@ -3,4 +3,4 @@
 #include "Structures.h"
 
 TMVector3 ComputeNormalVector(TMVector3 v1, TMVector3 v2, TMVector3 v3);
-TMVector3* TMVector3Cross(TMVector3* pOut, const TMVector3* pV1, const TMVector3* pV2);
+TMVector3 TMVector3Cross(const TMVector3* pV1, const TMVector3* pV2);
