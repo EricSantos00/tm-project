7dc67d8e01af9f87bc1662fa8c7b0beb0979f210
FREEDOM <56805685+wsphantom@users.noreply.github.com>
2020-08-01 19:55:04 -0300

    Started TMSelectCharScene

--- Projects/TMProject/TMSelectCharScene.cpp
+++ Projects/TMProject/TMSelectCharScene.cpp
@@ -1,8 +1,44 @@
 #include "pch.h"
 #include "TMSelectCharScene.h"
+#include "TMGlobal.h"
 
-TMSelectCharScene::TMSelectCharScene()
+TMSelectCharScene::TMSelectCharScene() :
+	m_vecSelPos{},
+	m_vecCrePos{}
 {
+	g_ClipFar = 70.0f;
+	m_dwStartRise = 0;
+	m_dwStartSet = 0;
+	m_dwCStartRise = 0;
+	m_dwCStartSet = 0;
+	m_dwLastMoveTime = 0;
+	m_bMovingNow = 0;
+	m_pBtnLogin = nullptr;
+	m_pBtnCancel = nullptr;
+	m_pBtnDelete = nullptr;
+	m_pBtnCreate = nullptr;
+	m_pRename = nullptr;
+	m_pEditRename = nullptr;
+	m_pPWEdit = nullptr;
+	m_pInputPWPanel = nullptr;
+	m_bShowNameLabel = 0;
+	m_dwLastClickLoginBtnTime = 0;
+	m_dwLastClickCreateBtnTime = 0;
+	m_fFocusHeight = 1.0f;
+	m_bAni = 0;
+	m_vecSelPos = TMVector2{ 2052.0f, 2052.0f };
+	m_vecCrePos = TMVector2{ 2080.0f, 2080.0f };
+	g_pObjectManager->m_cCharacterSlot = -1;
+	g_pObjectManager->m_usWarGuild = -1;
+	g_pObjectManager->m_usAllyGuild = -1;
+	m_pFocused = nullptr;
+
+	for (int i = 0; i < 4; ++i)
+		m_pHuman[i] = nullptr;
+
+	m_eSceneType = ESCENE_TYPE::ESCENE_SELCHAR;
+	m_bSelect = 1;
+	m_pAccountLockTime = 0;
 }
 
 TMSelectCharScene::~TMSelectCharScene()
