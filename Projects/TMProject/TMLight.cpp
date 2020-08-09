a676d4927b21b1b7e1c071b7b1979ab23ae1b4d1
FREEDOM <56805685+wsphantom@users.noreply.github.com>
2020-08-09 19:06:59 -0300

    Decompilation of TMLight::IsVisible finished

--- Projects/TMProject/TMLight.cpp
+++ Projects/TMProject/TMLight.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "TMLight.h"
+#include "TMCamera.h"
 #include "TMGlobal.h"
 
 unsigned int TMLight::m_dwBaseLightIndex = 2;
@@ -92,7 +93,70 @@ int TMLight::IsVisible()
 		return 1;
 	}
 	
-	// TODO
+	TMCamera* pCamera = g_pObjectManager->m_pCamera;
 
+	TMVector3 vecCam{ pCamera->m_cameraPos };
+
+	float fSightLen = pCamera->m_fSightLength;
+
+	if (fSightLen < 5.0f)
+		fSightLen = 5.0f;
+
+	float fL = (fSightLen * 0.80000001f) * 2.0f;
+	float fDXY = vecCam.z - vecCam.x;
+	float fAXY = vecCam.z + vecCam.x;
+	float fMore = 1.0f;
+	float fLen = 1.0f;
+
+	if (pCamera->m_fVerticalAngle >= -0.39269909f)
+	{
+		fMore = 1.6f;
+		fL = 7.1999998f;
+		fLen = 1.4f;
+		fDXY -= 2.0f;
+		fAXY += 2.0f;
+	}
+
+	float fRadius = m_Light.Range;
+
+	if (m_vecPosition.y < ((m_vecPosition.x + fDXY) + ((fMore * fL) / 1.414f)) &&
+		m_vecPosition.y > (((m_vecPosition.x + fDXY) + (fL / 1.414f)) - (fL * 1.414f)) &&
+		m_vecPosition.y > (-m_vecPosition.x + fAXY) &&
+		m_vecPosition.y < ((-m_vecPosition.x + fAXY) + (((fMore * 1.414f) * fL) * fLen)))
+	{
+		m_bVisible = 1;
+		return 1;
+	}
+
+	if (pCamera->m_fVerticalAngle < -0.39269909f)
+	{
+		if ((m_vecPosition.y + fRadius) < ((m_vecPosition.x + fDXY) + (fL / 1.414f)) &&
+			(m_vecPosition.y + fRadius) > (((m_vecPosition.x + fDXY) + (fL / 1.414f)) - (fL * 1.414f)) &&
+			(m_vecPosition.y + fRadius) > (-m_vecPosition.x + fAXY) &&
+			(m_vecPosition.y + fRadius) < ((-m_vecPosition.x + fAXY) + ((fL * 1.414f) * fLen)))
+		{
+			m_bVisible = 1;
+			return 1;
+		}
+
+		if ((m_vecPosition.y - fRadius) < ((m_vecPosition.x + fDXY) + (fL / 1.414f)) &&
+			(m_vecPosition.y - fRadius) > (((m_vecPosition.x + fDXY) + (fL / 1.414f)) - (fL * 1.414f)) &&
+			(m_vecPosition.y - fRadius) > (-m_vecPosition.x + fAXY) &&
+			(m_vecPosition.y - fRadius) < ((-m_vecPosition.x + fAXY) + ((fL * 1.414f) * fLen)))
+		{
+			m_bVisible = 1;
+			return 1;
+		}
+	}
+	else if ((m_vecPosition.y - fRadius) < ((m_vecPosition.x + fDXY) + ((fMore * fL) / 1.414f)) &&
+		(m_vecPosition.y - fRadius) > (((m_vecPosition.x + fDXY) + (fL / 1.414f)) - (fL * 1.414f)) &&
+		(m_vecPosition.y - fRadius) > (-m_vecPosition.x + fAXY) &&
+		(m_vecPosition.y - fRadius) < ((-m_vecPosition.x + fAXY) + (((fMore * 1.414f) * fL) * fLen)))
+	{
+		m_bVisible = 1;
+		return 1;
+	}
+
+	m_bVisible = 0;
 	return 0;
 }
