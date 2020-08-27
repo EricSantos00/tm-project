13748bbc237af29c5076e4cb7e6bb166ba807684
FREEDOM <56805685+wsphantom@users.noreply.github.com>
2020-08-27 17:01:42 -0300

    Decompiled TMEffectMeshRotate::Render

--- Projects/TMProject/TMEffectMeshRotate.cpp
+++ Projects/TMProject/TMEffectMeshRotate.cpp
@@ -2,6 +2,7 @@
 #include "TMObject.h"
 #include "TMEffectBillBoard.h"
 #include "TMEffectMeshRotate.h"
+#include "TMMesh.h"
 #include "TMGlobal.h"
 
 TMEffectMeshRotate::TMEffectMeshRotate(TMVector3 vecPosition, int nType, TMObject* pOwner, int bFire, int bCenter) :
@@ -64,11 +65,113 @@ TMEffectMeshRotate::TMEffectMeshRotate(TMVector3 vecPosition, int nType, TMObjec
 
 TMEffectMeshRotate::~TMEffectMeshRotate()
 {
+	if (m_pBillBoard != nullptr)
+		g_pObjectManager->DeleteObject(m_pBillBoard);
 }
 
 int TMEffectMeshRotate::Render()
 {
-	return 0;
+	if (g_bHideEffect == 1)
+		return 1;
+	if (!m_bVisible)
+		return 0;
+
+	TMMesh* pMesh{};
+
+	g_pDevice->SetRenderState(D3DRS_ALPHABLENDENABLE, 1u);
+	g_pDevice->SetRenderState(D3DRS_ALPHATESTENABLE, 0);
+	g_pDevice->SetRenderState(D3DRS_ZWRITEENABLE, 0);
+	g_pDevice->SetRenderState(D3DRS_LIGHTING, 0);
+	g_pDevice->SetRenderState(D3DRS_ALPHAFUNC, 8u);
+
+	if (m_efAlphaType == EEFFECT_ALPHATYPE::EF_BRIGHT)
+	{
+		pMesh = g_pMeshManager->GetCommonMesh(m_nMeshIndex, 1, 180000);
+
+		if (pMesh == nullptr)
+			return 0;
+
+		g_pDevice->SetRenderState(D3DRS_FOGENABLE, 0);
+		g_pDevice->SetRenderState(D3DRS_CULLMODE, 1u);
+		g_pDevice->SetRenderState(D3DRS_SRCBLEND, 5u);
+		g_pDevice->SetRenderState(D3DRS_DESTBLEND, 2u);
+
+		RDLVERTEX* pVertex;
+		D3DVERTEXBUFFER_DESC vDesc;
+
+		pMesh->m_pVB->GetDesc(&vDesc);
+		pMesh->m_pVB->Lock(0, 0, (void**)&pVertex, 0);
+
+		int nCount = vDesc.Size / 24;
+
+		float fAlpha = 1.0f;
+
+		if (m_nType == 1)
+			fAlpha = 1.0f;
+		else
+			fAlpha = sinf(m_fScale * 3.1415927f);
+
+		unsigned int dwA = (unsigned int)((float)m_dwA * fAlpha);
+		unsigned int dwR = (unsigned int)((float)m_dwR * fAlpha);
+		unsigned int dwG = (unsigned int)((float)m_dwG * fAlpha);
+		unsigned int dwB = (unsigned int)((float)m_dwB * fAlpha);
+		unsigned int dwColor = dwB | (dwG << 8) | (dwR << 16) | (dwA << 24);
+
+		for (int i = 0; i < nCount; ++i)
+			pVertex[i].diffuse = dwColor;
+
+		pMesh->m_pVB->Unlock();
+	}
+	else
+	{
+		pMesh = g_pMeshManager->GetCommonMesh(m_nMeshIndex, 0, 180000);
+
+		if (pMesh == nullptr)
+			return 0;
+
+		g_pDevice->SetRenderState(D3DRS_SRCBLEND, 5u);
+		g_pDevice->SetRenderState(D3DRS_DESTBLEND, 6u);
+		g_pDevice->SetTextureStageState(0, D3DTSS_ALPHAOP, 4u);
+		g_pDevice->SetRenderState(D3DRS_LIGHTING, 1u);
+		g_pDevice->SetRenderState(D3DRS_FOGENABLE, g_pDevice->m_bFog);
+
+		D3DCOLORVALUE color = g_pCurrentScene->GroundGetColor(TMVector2{ m_vecPosition.x, m_vecPosition.z });
+		color.r = (0.2f * color.r) + (g_pDevice->m_colorLight.r * 0.30000001f);
+		color.g = (0.2f * color.r) + (g_pDevice->m_colorLight.g * 0.30000001f);
+		color.b = (0.2f * color.r) + (g_pDevice->m_colorLight.b * 0.30000001f);
+		color.a = 1.0f;
+
+		_D3DMATERIAL9 materials{};
+		materials.Ambient.r = color.r;
+		materials.Ambient.g = color.g;
+		materials.Ambient.b = color.b;
+		materials.Ambient.a = color.a;
+		materials.Diffuse.r = color.r;
+		materials.Diffuse.g = color.g;
+		materials.Diffuse.b = color.b;
+		materials.Diffuse.a = color.a;
+		materials.Emissive.r = color.r;
+		materials.Emissive.g = color.g;
+		materials.Emissive.b = color.b;
+		materials.Emissive.a = color.a;
+		materials.Specular.r = 1.0f;
+		materials.Specular.g = 1.0f;
+		materials.Specular.b = 1.0f;
+		g_pDevice->m_pd3dDevice->SetMaterial(&materials);
+	}
+
+	if (pMesh == nullptr)
+		return 0;
+
+	pMesh->m_fScaleV = m_fScale;
+	pMesh->m_fScaleH = m_fScale;
+
+	if (m_nType == 1)
+		pMesh->Render(m_vecPosition.x, m_vecPosition.y, m_vecPosition.z, m_fAngle, m_fAngle2, 1.5707964f, 0, 0);
+	else
+		pMesh->Render(m_vecPosition.x, m_vecPosition.y, m_vecPosition.z, m_fAngle, 0, 1.5707964f, 0, 0);
+
+	return 1;
 }
 
 int TMEffectMeshRotate::FrameMove(unsigned int dwServerTime)
