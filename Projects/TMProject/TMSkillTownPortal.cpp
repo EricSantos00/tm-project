b1ca43cd3450a90b92f8b91105494b46b7152427
FREEDOM <56805685+wsphantom@users.noreply.github.com>
2020-08-28 01:20:08 -0300

    TMSkillTownPortal started

--- Projects/TMProject/TMSkillTownPortal.cpp
+++ Projects/TMProject/TMSkillTownPortal.cpp
@@ -1,8 +1,30 @@
 #include "pch.h"
 #include "TMSkillTownPortal.h"
+#include "TMGlobal.h"
 
 TMSkillTownPortal::TMSkillTownPortal(TMVector3 vecPosition, int nType)
 {
+    m_dwLifeTime = 1000;
+    m_fAngle = 0.0f;
+    m_fProgress = 0.0f;
+    m_vecPosition = vecPosition;
+    m_nType = nType;
+    m_dwStartTime = g_pTimerManager->GetServerTime();
+    m_dwLastTime = 0;
+
+    if (m_nType == 2)
+        SetColor(0x55FF);
+    else if (m_nType == 3)
+        SetColor(0xFF5500);
+    else
+        SetColor(0xFFFFFFFF);
+    
+    if (g_pSoundManager)
+    {
+        auto pSoundData = g_pSoundManager->GetSoundData(159);
+        if (pSoundData)
+            pSoundData->Play(0, 0);
+    }
 }
 
 TMSkillTownPortal::~TMSkillTownPortal()
