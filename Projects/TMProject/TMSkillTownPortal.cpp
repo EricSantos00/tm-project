3cb9ac789036d22749380d0b6184f3bd6277b3aa
FREEDOM <56805685+wsphantom@users.noreply.github.com>
2020-08-28 01:33:16 -0300

    Decompiled TMSkillTownPortal::Render and TMSkillTownPortal::IsVisible

--- Projects/TMProject/TMSkillTownPortal.cpp
+++ Projects/TMProject/TMSkillTownPortal.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "TMSkillTownPortal.h"
+#include "TMMesh.h"
 #include "TMGlobal.h"
 
 TMSkillTownPortal::TMSkillTownPortal(TMVector3 vecPosition, int nType)
@@ -33,12 +34,47 @@ TMSkillTownPortal::~TMSkillTownPortal()
 
 int TMSkillTownPortal::Render()
 {
-	return 0;
+    if (m_fProgress < 0.050000001f)
+        return 1;
+
+    if (m_bVisible == 1)
+    {
+        TMMesh* pMesh = g_pMeshManager->GetCommonMesh(703, 1, 180000);
+
+        if (!pMesh)
+            return 0;
+
+        g_pDevice->SetRenderState(D3DRS_ALPHABLENDENABLE, 1u);
+        g_pDevice->SetRenderState(D3DRS_CULLMODE, 1u);
+        g_pDevice->SetRenderState(D3DRS_SRCBLEND, 5u);
+        g_pDevice->SetRenderState(D3DRS_DESTBLEND, 2u);
+        g_pDevice->SetRenderState(D3DRS_ALPHAFUNC, 8u);
+        g_pDevice->SetRenderState(D3DRS_ALPHATESTENABLE, 0);
+        g_pDevice->SetRenderState(D3DRS_ZWRITEENABLE, 0);
+        g_pDevice->SetRenderState(D3DRS_LIGHTING, 0);
+
+        pMesh->m_fScaleV = m_fAngle;
+        pMesh->Render(m_vecPosition.x, m_vecPosition.y, m_vecPosition.z, 0, 0, 1.5707964f, 0, 0);
+
+        g_pDevice->SetRenderState(D3DRS_CULLMODE, 3u);
+        g_pDevice->SetRenderState(D3DRS_LIGHTING, 1u);
+        g_pDevice->SetRenderState(D3DRS_SRCBLEND, 2u);
+        g_pDevice->SetRenderState(D3DRS_ALPHAFUNC, 7u);
+        g_pDevice->SetRenderState(D3DRS_ALPHATESTENABLE, 1u);
+        g_pDevice->SetRenderState(D3DRS_ZWRITEENABLE, 1u);
+        g_pDevice->SetRenderState(D3DRS_DESTBLEND, 6u);
+    }
+    return 1;
 }
 
 int TMSkillTownPortal::IsVisible()
 {
-	return 0;
+    TMMesh* pMesh = g_pMeshManager->GetCommonMesh(703, 1, 180000);
+    if (pMesh)
+        m_fRadius = pMesh->m_fRadius;
+    else
+        m_fRadius = 1.0f;
+    return TMEffect::IsVisible();
 }
 
 int TMSkillTownPortal::FrameMove(unsigned int dwServerTime)
